4a4a8b98ec5f6cde60f013f70b0db4660bca7703
BeaverAss <tolik_morozov_98@mail.ru>
2022-07-18 13:03:09 +0300

    wrote app

---
+++ src/components/Profile/ProfileContainer.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+import Profile from "./Profile";
+import {connect} from "react-redux";
+import {getUserProfile} from "../../redux/profile-reducer";
+import {withAuthRedirect} from "../../hoc/withAuthRedirect";
+import {compose} from "redux";
+import {withRouter} from "../../hoc/WithRouter";
+
+class ProfileContainer extends React.Component {
+
+    componentDidMount() {
+        let userId = this.props.router.params.userId;
+        if(!userId) {
+            userId = 24675;
+        }
+
+        this.props.getUserProfile(userId);
+    }
+
+    render() {
+
+
+        return (
+             <Profile {...this.props} profile={this.props.profile}/>
+        );
+
+    }
+
+}
+let mapStateToProps = (state) => ({
+    profile: state.profilePage.profile,
+});
+
+
+export default compose(
+
+    connect(mapStateToProps, { getUserProfile }),
+    withAuthRedirect,
+    withRouter
+)(ProfileContainer);
+
